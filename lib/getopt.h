3ced373ec0c2e287d8092c1fd2d5a30e211282c3
Eric Gallager <egall@gwmail.gwu.edu>
2023-10-23 05:45:19 -0400

    update some autotools stuff

--- lib/getopt.h
+++ lib/getopt.h
@@ -100,8 +100,8 @@ struct option
 #define required_argument	1
 #define optional_argument	2
 
-#if defined (__STDC__) && __STDC__
-#ifdef __GNU_LIBRARY__
+#if defined(__STDC__) && __STDC__
+#if defined(__GNU_LIBRARY__) && (!defined(HAVE_DECL_GETOPT) || !HAVE_DECL_GETOPT)
 /* Many other libraries have conflicting prototypes for getopt, with
    differences in the consts, in stdlib.h.  To avoid compilation
    errors, only prototype getopt for the GNU C library.  */
